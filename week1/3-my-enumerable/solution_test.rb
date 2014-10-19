66b78f01d2ff986351bdf98f37161d32feab9404
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-10-19 12:44:39 +0300

    Introduce MyEnumerable problem

---
+++ week1/3-my-enumerable/solution_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+
+require_relative 'solution'
+
+class SolutionTest < Minitest::Test
+  class Collection
+    include MyEnumerable
+
+    def initialize(*data)
+      @data = data
+    end
+
+    def each(&block)
+      @data.each(&block)
+    end
+  end
+
+  def test_map
+    collection = Collection.new(*1..5)
+
+    assert_equals [2, 3, 4, 5, 6], collection.map(&:succ)
+  end
+
+  def test_filter
+    collection = Collection.new(*1..10)
+
+    assert_equals [1, 3, 5, 7, 9], collection.filter(&:odd?)
+  end
+
+  def test_reject
+    collection = Collection.new(*1..10)
+
+    assert_equals [1, 3, 5, 7, 9], collection.reject(&:even?)
+  end
+
+  def test_reduce
+    collection = Collection.new(*1..10)
+
+    assert_equals 55, collection.reduce(0) { |sum, n| sum + n }
+  end
+
+  def test_include?
+    collection = Collection.new(*1..10)
+
+    assert_equals true, collection.include?(5)
+  end
+end
